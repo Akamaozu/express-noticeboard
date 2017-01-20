62045d40d1503b663daa709e18c9ef9d2c31820b
akamaozu <uzo@designbymobius.ca>
2017-01-20 12:27:02 -0500

    added ability to pass a noticeboard to middleware wrapper
    also wrote much-needed comments

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 module.exports = function( config ){
 
-  if( !config || Object.prototype.toString.call( config ) !== '[object Object]' ) config = {};
-  
-  var Noticeboard = require('cjs-noticeboard'),
-      noticeboard = new Noticeboard( config ),
-      middleware = middleware_wrapper;
+  var middleware = middleware_wrapper,
+      noticeboard;
 
+  if( !config || Object.prototype.toString.call( config ) !== '[object Object]' ) config = {};
+
+  // use created noticeboard
+  if( config.hasOwnProperty( 'noticeboard' ) ) {
+
+    noticeboard = config.noticeboard;
+  }
+
+  // create new noticeboard instance
+  else {
+
+    var Noticeboard = require('cjs-noticeboard');    
+    noticeboard = new Noticeboard( config );    
+  }
+  
+  // shim middleware with noticeboard methods and properties
+
+  // - sometimes watching or publishing outside routes is needed
+  // - this glues noticeboard methods and props to middleware
+  // - that way it feels and acts like a real noticeboard. kinda.
+  
   for( var prop in noticeboard ){
 
+    // shim functions
     if( typeof noticeboard[ prop ] === 'function' ){
 
       if( prop === 'constructor' ){
-
         middleware.constructor = noticeboard.constructor;
         continue;
       }
@@ -19,26 +37,25 @@ module.exports = function( config ){
       middleware[ prop ] = function( prop ){
 
         return function(){
-
-          noticeboard[ prop ].apply( noticeboard, arguments ) 
+          noticeboard[ prop ].apply( noticeboard, arguments );
         }
-        
+
       }( prop );
     }
 
     else switch( Object.prototype.toString.call( noticeboard[ prop ] ) ){
 
+      // shim objects
       case '[object Object]':
 
         middleware[ prop ] = noticeboard[ prop ];
-
       break;
     }
   }
 
   return middleware;
 
-  function middleware_wrapper( req, res, next){
+  function middleware_wrapper( req, res, next ){
 
     req.noticeboard = noticeboard;
     next();
